c4f6f405914c9a4c605dc9ec00742aaa16b0ce52
Dave Shapiro <dss.shapiro@gmail.com>
2017-10-28 12:36:38 -0500

    running both adapters in test

--- spec/uber-s3/bucket_spec.rb
+++ spec/uber-s3/bucket_spec.rb
@@ -1,10 +1,10 @@
 require 'spec_helper'
 
 describe UberS3::Bucket do
-  # [:net_http, :em_http_fibered].each do |connection_adapter|
-  [:net_http].each do |connection_adapter|
+  [:net_http, :em_http_fibered].each do |connection_adapter|
+  # [:net_http].each do |connection_adapter|
 
-    
+
     context "#{connection_adapter}: Bucket" do
       let(:s3) do
         UberS3.new({
@@ -17,22 +17,23 @@ describe UberS3::Bucket do
 
       it 'iterates objects in a bucket' do
         spec(s3) do
-          folder = 'bucket_spec'
-          
+          folder = '/bucket_spec'
+
           s3.store(folder+'/file1.txt', '1')
           s3.store(folder+'/file2.txt', '2')
           s3.store(folder+'/file3.txt', '3')
-          
+
           objs = []
           s3.objects('/bucket_spec').each do |obj|
+            puts obj
             objs << obj
           end
-          
+
           objs.length.should == 3
         end
       end
 
     end
-    
+
   end
 end
--- spec/uber-s3/object_spec.rb
+++ spec/uber-s3/object_spec.rb
@@ -1,8 +1,8 @@
 require 'spec_helper'
 
 describe UberS3::Object do
-  # [:net_http, :em_http_fibered].each do |connection_adapter|
-  [:net_http].each do |connection_adapter|
+  [:net_http, :em_http_fibered].each do |connection_adapter|
+  # [:net_http].each do |connection_adapter|
 
     context "#{connection_adapter}: Object" do
       let(:s3) do
@@ -15,55 +15,55 @@ describe UberS3::Object do
       end
 
       let(:obj) { UberS3::Object.new(s3.bucket, '/test.txt', 'heyo') }
-      
+
       it 'storing and loading an object' do
         spec(s3) do
           obj.save.should == true
           obj.exists?.should == true
-      
+
           #--
-      
+
           key = 'test.txt'
           value = 'testing 1234...'
-          
+
           s3.store(key, value).should == true
-      
+
           s3.exists?(key).should == true
           s3.object(key).exists?.should == true
           s3.exists?('asdfasdfasdf').should == false
-      
+
           s3[key].value.should == value
-      
+
           s3[key].delete.should == true
           s3[key].exists?.should == false
         end
       end
-      
+
       it 'has access level control' do
         spec(s3) do
           obj.access = :public_read
           obj.save.should == true
         end
       end
-      
+
       it 'perform md5 integrity check' do
         spec(s3) do
-          obj.content_md5 = Digest::MD5.hexdigest(obj.value)          
+          obj.content_md5 = Digest::MD5.hexdigest(obj.value)
           obj.save.should == true
         end
       end
 
       it 'encode the data with gzip' do
-        spec(s3) do        
+        spec(s3) do
           key = 'gzip_test.txt'
           value = 'testing 1234...'*256
-        
+
           s3.store(key, value, { :gzip => true })
-        
+
           # Uber S3 client will auto-decode ...
           gzipped_data = s3[key].value
           gzipped_data.bytesize.should == value.bytesize
-          
+
           # But, let's make sure on the server it's the small size
           header = s3.connection.head(key).header
           content_length = [header['content-length']].flatten.first.to_i
@@ -111,8 +111,8 @@ describe UberS3::Object do
           obj.meta['content_md5'].should == meta['content_md5']
         end
       end
-      
+
     end
-    
+
   end
 end
